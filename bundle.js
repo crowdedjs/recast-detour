781b58f0c3f16c597dba8f29e8698df56837cf77
B. Ricks <bricks@unomaha.edu>
2020-10-17 17:40:41 -0500

    feat: Change to umd

--- bundle.js
+++ bundle.js
@@ -1,5 +1,8 @@
-var recastdetourjs = (function (exports) {
-	'use strict';
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.recastdetourjs = {}));
+}(this, (function (exports) { 'use strict';
 
 	/*
 	Copyright (c) 2009-2010 Mikko Mononen memon@inside.org
@@ -17710,6 +17713,6 @@ var recastdetourjs = (function (exports) {
 	exports.ObstacleAvoidanceParams = ObstacleAvoidanceParams;
 	exports.RecastTestMeshBuilder = RecastTestMeshBuilder;
 
-	return exports;
+	Object.defineProperty(exports, '__esModule', { value: true });
 
-}({}));
+})));
