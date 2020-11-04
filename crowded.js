9c7c52c5c6534b709848aa77811f57ce6b9ca81c
B. Ricks <bricks@unomaha.edu>
2020-11-04 15:55:21 -0600

    fix: rollup dist not src

--- crowded.js
+++ crowded.js
@@ -10389,11 +10389,11 @@
 	 */
 	class BVNode {
 		/** Minimum bounds of the node's AABB. [(x, y, z)] */
-	bmin = new Array(3);
+		bmin = new Array(3);
 		/** Maximum bounds of the node's AABB. [(x, y, z)] */
-	bmax = new Array(3);
+		bmax = new Array(3);
 		/** The node's index. (Negative for escape sequence.) */
-	 i = 0;
+		i = 0;
 	}
 
 	/*
